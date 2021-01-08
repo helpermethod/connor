1ce9f798946e702311c54f1ff631dd7d4d94a105
helpermethod <oliver@sdkman.io>
2021-01-08 16:50:12 +0100

    Add Key class

---
+++ src/main/java/com/github/helpermethod/kafka/connect/reset/Key.java
@@ -0,0 +1,19 @@
+package com.github.helpermethod.kafka.connect.reset;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.ARRAY;
+
+@JsonFormat(shape = ARRAY)
+@JsonPropertyOrder(alphabetic = true)
+class Key {
+    final String connector;
+
+    @JsonCreator
+    Key(@JsonProperty("connector") String connector) {
+        this.connector = connector;
+    }
+}
