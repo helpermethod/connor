110b3e9fc2b04aa582e86dcae88d53052bb0e9b5
Oliver Weiler <oliver@sdkman.io>
2021-09-02 17:44:25 +0200

    Use Prettier for source code formatting (#52)

--- src/main/java/com/github/helpermethod/connor/substitutions/NoopMetricsReporter.java
+++ src/main/java/com/github/helpermethod/connor/substitutions/NoopMetricsReporter.java
@@ -2,46 +2,39 @@ package com.github.helpermethod.connor.substitutions;
 
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import java.util.List;
+import java.util.Map;
 import org.apache.kafka.common.metrics.JmxReporter;
 import org.apache.kafka.common.metrics.KafkaMetric;
 import org.apache.kafka.common.metrics.MetricsReporter;
 
-import java.util.List;
-import java.util.Map;
-
 @Substitute
 @TargetClass(JmxReporter.class)
 final class NoopMetricsReporter implements MetricsReporter {
-    @Substitute
-    public NoopMetricsReporter() {
-    }
 
     @Substitute
-    public NoopMetricsReporter(String prefix) {
-    }
+    public NoopMetricsReporter() {}
 
     @Substitute
-    @Override
-    public void init(List<KafkaMetric> metrics) {
-    }
+    public NoopMetricsReporter(String prefix) {}
 
     @Substitute
     @Override
-    public void metricChange(KafkaMetric metric) {
-    }
+    public void init(List<KafkaMetric> metrics) {}
 
     @Substitute
     @Override
-    public void metricRemoval(KafkaMetric metric) {
-    }
+    public void metricChange(KafkaMetric metric) {}
 
     @Substitute
     @Override
-    public void close() {
-    }
+    public void metricRemoval(KafkaMetric metric) {}
 
     @Substitute
     @Override
-    public void configure(Map<String, ?> configs) {
-    }
+    public void close() {}
+
+    @Substitute
+    @Override
+    public void configure(Map<String, ?> configs) {}
 }
